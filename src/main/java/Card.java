7aeb8c11c0e256c5f244327c034f5bf93232d292
albertopjk <alberto.perez-jacome@menloschool.org>
2025-12-01 14:57:45 -0800

    Made the files and basics

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,8 +1,41 @@
 public class Card {
-    public static void main(String[] args){
-        String rank = "";
 
+    private String rank;
+    private String suit;
+    private int value;
+
+    //Constructor real quick
+    public Card(String rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
     }
 
+    public String getRank() {
+        return rank;
+    }
 
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return (rank + " of " + suit);
+    }
 }
