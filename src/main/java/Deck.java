f2482b2aada39b034a17fcb3e7ecec46b0b1e385
albertopjk <alberto.perez-jacome@menloschool.org>
2025-12-03 12:46:41 -0800

    Added the Deck and Player Class, will begin the Game Class

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    ArrayList<Card> cards = new ArrayList<>();
+    private int cardsLeft;
+
+
+    public Deck(ArrayList<String> rank, ArrayList<String> suit, ArrayList<Integer> value){
+        // Generates cards with a nested for loop using ranks, suit, value
+        for(int i = 0; i < rank.size(); i++){
+            for(int j = 0; j < suit.size(); j++){
+                Card card = new Card(rank.get(i), suit.get(j), value.get(i));
+
+                cards.add(card);
+                cardsLeft++;
+            }
+        }
+
+    }
+
+    public boolean isEmpty() {
+        return cardsLeft == 0;
+    }
+
+    public int getCardsLeft(){
+        return cardsLeft;
+    }
+
+    public Card deal(){
+        if(cardsLeft == 0){
+            return null;
+        }
+        cardsLeft--;
+        return cards.get(cardsLeft + 1);
+
+    }
+
+    public void shuffle(){
+        for(int i = cards.size(); i > 0; i--){
+            int random = (int)(Math.random()*i);
+            Card copy = cards.get(i);
+            cards.set(i, cards.get(random));
+            cards.set(random, copy);
+        }
+    }
+
+
+
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,47 @@
+import java.util.ArrayList;
+
 public class Player {
+
+    ArrayList<Card> hand = new ArrayList<>();
+    private int points;
+    private String name;
+
+    public Player(String name){
+        this.name = name;
+        this.points = 0;
+    }
+
+    public Player(String name, ArrayList<Card> hand){
+        this.name = name;
+        this.hand = hand;
+        this.points = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void addPoints(int numPoints){
+        points += numPoints;
+    }
+
+    public void addCard(Card card){
+        hand.add(card);
+    }
+
+    public String toString(){
+        return name + " has " + points + " points.\n" + name + "'s cards: " + hand;
+
+    }
+
+
+
 }
